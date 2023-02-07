76a94022f25e0f770b621e6c29087fcbcf471ac5
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-02-07 14:51:41 +0100

    MyNav component

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -2,14 +2,15 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 
 const MyNav = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Browse</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
