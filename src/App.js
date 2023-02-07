92503099b1b506dfbb1632bab7e0df8d51c6a85b
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-02-07 15:31:17 +0100

    Welcome component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 import MyNav from "./components/MyNav";
+import Welcome from "./components/Welcome";
 import MyFooter from "./components/MyFooter";
 
 function App() {
   return (
     <div className="App">
       <MyNav />
+      <Welcome />
       <MyFooter />
     </div>
   );
